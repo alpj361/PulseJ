6912fabc95cef1bdd69ce3cbb434e9f14a62e676
alpj361 <alpj361@gmail.com>
2025-06-29 03:20:54 -0600

    Fix Material-UI Theme Provider Setup

--- src/main.tsx
+++ src/main.tsx
@@ -83,10 +83,9 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
-    </ThemeProvider> */}
-    <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
